96ddea13fcd22f568bf40a7b08967c9939f0b2d4
Joe Averbukh <joeaverbukh@gmail.com>
2023-09-29 16:46:11 -0400

    MoveFast

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 
-import App from "./BeAHost/poster";
+import App from "./MoveFast/poster";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(<App />);
