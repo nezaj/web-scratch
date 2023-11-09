052d0edf6c90cf5b5418b38621c96f21565520fc
Joe Averbukh <joeaverbukh@gmail.com>
2023-11-09 12:59:22 -0500

    add scripts

---
+++ src/scripts/pgTest.js
@@ -0,0 +1,38 @@
+/* Quick script for running concurrent sql queries on pg */
+require('dotenv').config();
+
+const pg = require('@databases/pg');
+const createConnectionPool = pg.default;
+const sql = pg.sql;
+
+const POOL_SIZE = 50;
+const NUM_QUERIES = 400;
+
+function getPostgresConfig() {
+  return {
+    user: process.env.INSTANT_DB_USER,
+    database: process.env.INSTANT_DB_DATABASE,
+    password: process.env.INSTANT_DB_PASSWORD,
+    port: process.env.INSTANT_DB_PORT,
+    host: process.env.INSTANT_DB_HOST,
+    bigIntMode: 'bigint',
+    poolSize: POOL_SIZE,
+  }
+}
+
+const conn = createConnectionPool(getPostgresConfig())
+
+async function runQueries(conn) {
+  console.time('runQueries'); // Start timing
+
+  const queries = new Array(NUM_QUERIES).fill(null).map(() => conn.query(sql`SELECT 1`));
+  await Promise.all(queries);
+
+  console.timeEnd('runQueries'); // End timing and log the result
+}
+
+runQueries(conn).then(() => {
+  console.log('Finished running queries');
+}).catch(error => {
+  console.error('Error executing queries:', error);
+})
