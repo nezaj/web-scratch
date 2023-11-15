e64e2e4c5881e6a71b02ace95f9580a96c691119
Joe Averbukh <joeaverbukh@gmail.com>
2023-11-14 20:12:48 -0500

    More emails

---
+++ src/scripts/extractSummary.js
@@ -0,0 +1,71 @@
+/*
+  * Quick script for extractSummary summaries from shortform
+  * Usage: node src/scripts/extractSummarySummary.js
+  * */
+
+require('dotenv').config();
+
+const fs = require('fs');
+const path = require('path');
+
+function fetchSummary(url) {
+  return new Promise((resolve, reject) => {
+    fetch(url, {
+      "headers": {
+        "accept": "application/json, text/plain, */*",
+        "authorization": process.env.SHORTFORM_AUTH,
+        "sec-ch-ua": process.env.SHORTFORM_UA,
+        "sec-ch-ua-mobile": "?0",
+        "sec-ch-ua-platform": "\"macOS\"",
+        "x-sf-client": "11.8.0"
+      },
+      "referrer": "https://shortform.com/",
+      "referrerPolicy": "strict-origin-when-cross-origin",
+      "body": null,
+      "method": "GET",
+      "mode": "cors",
+      "credentials": "include"
+    }).then(response => response.json())
+      .then(jsonData => resolve(extractSummary(jsonData.data)))
+      .catch(error => reject(error))
+  })
+}
+
+function extractSummary(data) {
+  if (!data) { return }
+  const { cover_image, title, author, tags, url_slug } = data
+  const html = data.content[0].text
+  return {
+    cover_image: "https:" + cover_image,
+    title,
+    author,
+    tags,
+    url_slug,
+    html
+  }
+}
+
+const API_URL = "https://www.shortform.com/api/books"
+const slugs = [
+  "chip-war",
+  "the-design-of-everyday-things",
+  "the-innovator-s-dilemma",
+  "sex-at-dawn",
+  "the-hero-with-a-thousand-faces"
+]
+
+async function doWork() {
+  const summaries = await Promise.all(slugs.map(slug => fetchSummary(`${API_URL}/${slug}`)));
+  const jsonContent = JSON.stringify(summaries.filter(x => x), null, 2);
+  const outPath = path.join(__dirname, "bookSummaries.json");
+
+  fs.writeFile(outPath, jsonContent, 'utf8', (err) => {
+    if (err) {
+      console.log("An error occurred while writing JSON Object to File.");
+      return console.log(err);
+    }
+    console.log("Done!");
+  });
+}
+
+doWork();
--- src/scripts/sendEmail.js
+++ src/scripts/sendEmail.js
@@ -1,14 +1,24 @@
 /*
   * Quick script for sending emails via postmark
-  * Usage: node src/scripts/sendEmail.js
+  * Usage: npx @babel/node src/scripts/sendEmail.js
   * */
 
 require('dotenv').config();
+require('@babel/register')({
+  presets: ['@babel/preset-env', '@babel/preset-react'],
+  plugins: [
+    ['transform-require-ignore', {
+      extensions: ['.css']
+    }]
+  ],
+  extensions: [".js", ".jsx"],
+});
 
 const Postmark = require('postmark');
+const juice = require('juice');
 const ReactDOMServer = require('react-dom/server');
 const React = require('react');
-const MyComponent = require('@/BookSumary/index');
+const MyComponent = require('../BookSummary/index').default;
 
 // Replace 'your-server-token' with your actual Postmark server token
 const client = new Postmark.ServerClient(process.env.POSTMARK_SERVER_TOKEN);
@@ -16,13 +26,211 @@ const client = new Postmark.ServerClient(process.env.POSTMARK_SERVER_TOKEN);
 // Render your React component to a string
 const emailHtml = ReactDOMServer.renderToString(React.createElement(MyComponent));
 
+const cssStyles = `
+* {
+    box-sizing: border-box;
+    margin: 0;
+    padding: 0;
+ }
+
+html {
+    overflow-y: scroll;
+    height: 100%;
+    font: 100%/1.5 sans-serif;
+    word-wrap: break-word;
+    margin: 0 auto;
+    padding: 1.5em;
+ }
+
+@media (min-width: 768px) {
+    html {
+        font-size: 125%;
+        max-width: 42em;
+ } }
+
+h1, h2, h3, h4 {
+    margin: 2.5rem 0 1.5rem 0;
+    line-height: 1.25;
+    color: #333;
+ }
+
+a {
+    color: #fa6432;
+    text-decoration: none;
+ }
+a:hover, a:focus, a:active {
+    text-decoration: underline;
+ }
+
+p {
+    margin: 1em 0;
+    line-height: 1.5;
+ }
+p code {
+    background-color: #eee;
+    padding: 0.05em 0.2em;
+    border: 1px solid #ccc;
+ }
+
+ol, ul {
+    margin: 1em;
+ }
+ol li ol, ol li ul, ul li ol, ul li ul {
+    margin: 0 2em;
+ }
+ol li p, ul li p {
+    margin: 0;
+ }
+
+dl {
+    font-family: monospace, monospace;
+ }
+dl dt {
+    font-weight: bold;
+ }
+dl dd {
+    margin: -1em 0 1em 1em;
+ }
+
+img {
+    max-width: 100%;
+    display: block;
+    margin: 0 auto;
+    padding: 0.5em;
+ }
+
+blockquote {
+    padding-left: 1em;
+    font-style: italic;
+    border-left: solid 1px #fa6432;
+ }
+
+table {
+    font-size: 1rem;
+    text-align: left;
+    caption-side: bottom;
+    margin-bottom: 2em;
+ }
+table * {
+    border: none;
+ }
+table thead, table tr {
+    display: table;
+    table-layout: fixed;
+    width: 100%;
+ }
+table tr:nth-child(even) {
+    background-color: rgba(200, 200, 200, 0.2);
+ }
+table tbody {
+    display: block;
+    max-height: 70vh;
+    overflow-y: auto;
+ }
+table td, table th {
+    padding: 0.25em;
+ }
+
+table, .highlight > pre, pre.example {
+    max-height: 70vh;
+    margin: 1em 0;
+    padding: 1em;
+    overflow: auto;
+    font-size: 0.85rem;
+    font-family: monospace, monospace;
+    border: 1px dashed rgba(250, 100, 50, 0.5);
+}
+
+.title {
+    font-size: 2.5em;
+}
+
+.subtitle {
+    font-weight: normal;
+    font-size: 0.75em;
+    color: #666;
+}
+
+.tags {
+    margin-top: -1.5rem;
+    padding-bottom: 1.5em;
+}
+.tags li {
+    display: inline;
+    margin-right: 0.5em;
+}
+
+figure {
+    margin: 1em 0;
+}
+figure figcaption {
+    font-family: monospace, monospace;
+    font-size: 0.75em;
+    text-align: center;
+    color: grey;
+}
+
+.footnote-definition sup {
+    margin-left: -1.5em;
+    float: left;
+}
+
+.footnote-definition .footnote-body {
+    margin: 1em 0;
+    padding: 0 1em;
+    border: 1px dashed rgba(250, 100, 50, 0.3);
+    background-color: rgba(200, 200, 200, 0.2);
+}
+.footnote-definition .footnote-body p:only-child {
+    margin: 0.2em 0;
+}
+
+header {
+    display: flex;
+    justify-content: space-between;
+}
+header nav {
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+}
+header a + a {
+    margin-left: 1rem;
+}
+
+.preview {
+    margin-top: 1rem;
+    margin-bottom: 1rem; 
+}
+
+.title {
+    margin-top: 0.5rem;
+    margin-bottom: 0.5rem; 
+    font-size: 1.5rem;
+    line-height: 2rem; 
+    font-weight: 600; 
+}
+
+.tags {
+    margin-top: 0.5rem;
+    margin-bottom: 0.5rem;
+    font-size: 0.75rem;
+    line-height: 1rem; 
+    font-style: italic; 
+}
+`
+
+
 // Set up email parameters
+const htmlBody = juice.inlineContent(emailHtml, cssStyles);
 const email = {
-  "From": "joeaverbukh@gmail.com",
+  "From": "joe@instantdb.com",
   "To": "joeaverbukh@gmail.com",
   "Subject": "[TEST EMAIL] Book summary",
-  "HtmlBody": emailHtml,
+  "HtmlBody": htmlBody
 };
+
+// console.log(htmlBody)
 
 // Send the email through Postmark
 client.sendEmail(email, function(error, result) {
